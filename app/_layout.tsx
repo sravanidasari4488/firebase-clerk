51ca5abf5bcc96ecffdb698898de96a8536f1b60
sravanidasari4488 <sravanidasari4488@gmail.com>
2025-01-22 23:11:38 +0530

    clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from "react";
+import {Stack} from "expo-router/stack";
+import { tokenCache } from '@/cache'
+import { ClerkProvider } from '@clerk/clerk-expo';
+import  * as SecureStore from 'expo-secure-store';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+if (!publishableKey){
+  throw new Error(
+    "missing publishable key. please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
   );
 }
+function RootLayout(){
+  return(
+    <ClerkProvider publishableKey={publishableKey}>
+      <Stack screenOptions={{}}>
+        <Stack.Screen name="index" options={{headerShown: false}} />
+        <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+      </Stack>
+    </ClerkProvider>
+  );
+}
+
+export default RootLayout;
